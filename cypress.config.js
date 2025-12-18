ba0a5ba65cc8a6d97e629e56c74552c15382cf8b
Vitor Fantin <fantinfx@gmail.com>
2025-12-17 22:30:34 -0300

    estrutura básica pronta para automação

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "pk3pfc",
+  projectId: 'pk3pfc',
   e2e: {
+    baseUrl: 'https://www.certiqa-qazando.com',
     setupNodeEvents(on, config) {
-    baseUrl = 'https://www.certiqa-qazando.com/';
+      // plugins / eventos aqui
+      return config
     },
-  },
+  }
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,18 +8,14 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('btnLogin', () => {
+    cy.get('#root button.border').should('have.text', 'Login').click();
+});
+
+Cypress.Commands.add('loginConta', (email,senha) => {
+    cy.get('[name="email"]').type(email);
+    cy.get('[name="password"]').type(senha);
+    cy.get('.inline-flex').should('have.text', 'Entrar').click();
+    cy.get('.grid > .font-semibold').should('have.text', 'Login bem-sucedido')
+});
