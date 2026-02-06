3d9580ddb3c8de2dd57f1c281fd74b445c164c76
Rafael Dal Forno <rafa_fln15@hotmail.com>
2026-02-05 23:16:43 -0300

    Adicionando os testes automatizados na branch test/kan-91

--- cypress/e2e/tests/feature/kan-91-Fase5-Implementacao-Execucao-Testes/kan-91-execucao-testes.cy.js
+++ cypress/e2e/tests/feature/kan-91-Fase5-Implementacao-Execucao-Testes/kan-91-execucao-testes.cy.js
@@ -1,4 +1,7 @@
+/// <reference types="cypress" />
+
 import * as dashboardCtfl from "../../../../pages/page-dashboard-estudos-ctfl";
+import * as fase5Fase6Ctfl from "../../../../pages/page-dashboard-fase5-fase6-ctfl";
 
 // DADOS DE LOGIN
 // qa.admin@certiqa.com    QA@Test2024
@@ -9,13 +12,70 @@ import * as dashboardCtfl from "../../../../pages/page-dashboard-estudos-ctfl";
 describe('Kan-91 - Implementação e Execução de Testes', () => {
       beforeEach(() => {
             cy.visit('/');
-      });
-
-      it('KAN-91 Execução de Testes - Exemplo de Teste Simples', () => {
-            // Exemplo de teste simples para verificar o título da página inicial
             cy.btnLogin();
             cy.loginConta('grupo1-qazando@proton.me', 'grupo1qazando');
-            dashboardCtfl.funcAcessarPaginaCtfl();
+            fase5Fase6Ctfl.accessCertificationCtfl();
+      });
 
+      it('KAN-92 Descrição página "Conteúdo da Fase" - Fase 5', () => {
+            fase5Fase6Ctfl.acessarFase5();
+      });
+
+      it('KAN-93 Finalização do simulado ao excerder tempo máximo - Fase 5', () => {
+            fase5Fase6Ctfl.acessarFase5();
+            fase5Fase6Ctfl.iniciarQuiz();
+            cy.wait(120000);
+      });
+
+      it('KAN-94 Funcionalidade botão "Sair Quiz" - Fase 5', () => {
+            fase5Fase6Ctfl.acessarFase5();
+            fase5Fase6Ctfl.iniciarQuiz();
+            fase5Fase6Ctfl.sairQuiz();
+      });
+
+      it('KAN-95 Funcionalidade botão "Anterior" durante o simulado - Fase 5', () => {
+            fase5Fase6Ctfl.acessarFase5();
+            fase5Fase6Ctfl.iniciarQuiz();
+            fase5Fase6Ctfl.marcarOpcao3();
+            fase5Fase6Ctfl.botaoAnteriorQuiz();
+      });
+
+      it('KAN-96 Tela das respostas ao finalizar o simulado - Fase 5', () => {
+            fase5Fase6Ctfl.acessarFase5();
+            fase5Fase6Ctfl.iniciarQuiz();
+
+            // Marcar todas as respostas como a opção 2
+            for (let i = 0; i < 10; i++) {
+                  fase5Fase6Ctfl.marcarOpcao2();
+            }
+            
+            // Checar mensagens de finalização do quiz
+            fase5Fase6Ctfl.checarNotaFinal();
+      });
+
+      it('KAN-97 Funcionalidade do botão "Tentar Novamente" - Fase 5', () => {
+            fase5Fase6Ctfl.acessarFase5();
+            fase5Fase6Ctfl.iniciarQuiz();
+
+            // Marcar todas as respostas como a opção 2
+            for (let i = 0; i < 10; i++) {
+                  fase5Fase6Ctfl.marcarOpcao2();
+            }
+            
+            // Checar mensagens de finalização do quiz
+            fase5Fase6Ctfl.checarNotaFinal();
+
+            
+            // BOTÂO TENTAR NOVAMENTE NAO IMPLEMENTADO NA PAGINA ATUALMENTE
+      });
+
+      it.only('KAN-98 UI progressão ao avançar no simulado - Fase 5', () => {
+            fase5Fase6Ctfl.acessarFase5();
+            fase5Fase6Ctfl.iniciarQuiz();
+
+            for (let i = 0; i < 10; i++) {
+                  fase5Fase6Ctfl.verificarQuestaoAtual(i + 1);
+                  fase5Fase6Ctfl.marcarOpcao2();
+            }
       });
 });
---
+++ cypress/pages/page-dashboard-fase5-fase6-ctfl.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+
+export default {
+  accessCertificationCtfl(){
+    cy.get('nav > a[href="/certificacoes"]').click();
+    cy.get('a[href="/certificacoes/ctfl"]').click();
+    cy.validarURL('https://www.certiqa-qazando.com/certificacoes/ctfl');
+  },
+
+  acessarFase5(){
+    cy.get('a[href="/certificacoes/ctfl/fase/test-implementation"]').click();
+    cy.validarURL('https://www.certiqa-qazando.com/certificacoes/ctfl/fase/test-implementation');
+  },
+
+  iniciarQuiz(){
+    cy.get('.text-primary-foreground').click();
+    cy.get('.space-y-2 > .flex > :nth-child(1)').should('contain.text', 'Questão 1 de 10');
+  },
+
+  marcarOpcao2(){
+    cy.get('.space-y-2 > :nth-child(2) > .flex > span').click();
+    cy.get(':nth-child(2) > .flex > .bg-primary').click();
+  },
+
+  sairQuiz(){
+    cy.get('.pt-0 > :nth-child(2) > .flex > .border').click();
+    cy.validarURL('https://www.certiqa-qazando.com/certificacoes/ctfl/fase/test-implementation');
+  },
+
+  botaoAnteriorQuiz(){
+    cy.get('.space-y-2 > .flex > :nth-child(1)').should('contain.text', 'Questão 2 de 10');
+    cy.get('.pt-0 > :nth-child(2) > :nth-child(1)').click();
+    cy.get('.space-y-2 > .flex > :nth-child(1)').should('contain.text', 'Questão 1 de 10');
+  },
+
+  verificarQuestaoAtual(numeroQuestao){
+    cy.get('.space-y-2 > .flex > :nth-child(1)').should('contain.text', `Questão ${numeroQuestao} de 10`);
+  },
+
+  // Checagem de mensagens
+  checarMensagemTempoEsgotado(){
+    cy.get('.flex-col > .text-2xl').should('have.text', 'Resultado do Quiz - Quiz: Implementação e Execução');
+  },
+
+  checarNotaFinal(){
+    cy.get('.flex-col > .text-2xl').should('have.text', 'Resultado do Quiz - Quiz: Implementação e Execução');
+    cy.get('.mt-2').should('contain.text', 'Parabéns! Você passou!');
+  }
+
+}
